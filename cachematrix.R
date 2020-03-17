629950a31867c8a90b5aa94ea0a4dc40b8269ad3
Omar Rodrigues da Silva <omarrodriguesdasilva@gmail.com>
2020-03-16 23:49:17 -0300

    commit the ProgrammingAssignment2 - cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This makeCacheMatrix receives an invertible matrix and cache it
+## There are 4 embedded subfunctions defined: set, get, setinvm and getinvm
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinvm <- function(solve) m <<- solve
+        getinvm <- function() m
+        list(set = set, get = get,
+             setinvm = setinvm,
+             getinvm = getinvm)
 }
-
-
-## Write a short comment describing this function
-
+## cachesolve calculate the inverted matrix if has not done before
+## if the inverted matrix was calculated, it just returns the cached matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinvm()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinvm(m)
+        m
 }
